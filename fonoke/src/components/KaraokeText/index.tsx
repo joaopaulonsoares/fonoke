3a07c080f4ad1273417a469fd79e2dd7db0639af
Joao Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2023-01-13 22:33:25 -0300

    Introduce first version

---
+++ fonoke/src/components/KaraokeText/index.tsx
@@ -0,0 +1,49 @@
+import styles from '@/styles/Home.module.css'
+
+import AnimatedText from "react-animated-text-content";
+
+
+interface KaraokeInterface{
+  text:string
+
+}
+
+const vogais = ["a", "e", "i", "o", "u"]
+
+function removeAccentofVogal(text:string){       
+    let testChar = text + ''
+    testChar = testChar.toLowerCase();                                                         
+    testChar = testChar.replace(new RegExp('[ÁÀÂÃ]','gi'), 'a');
+    testChar = testChar.replace(new RegExp('[ÉÈÊ]','gi'), 'e');
+    testChar = testChar.replace(new RegExp('[ÍÌÎ]','gi'), 'i');
+    testChar = testChar.replace(new RegExp('[ÓÒÔÕ]','gi'), 'o');
+    testChar = testChar.replace(new RegExp('[ÚÙÛ]','gi'), 'u');
+    return testChar;                 
+}
+
+export function KaraokeText({text}:KaraokeInterface){
+
+  const triplicado = text.split('').map(function(char) {
+    if (vogais.indexOf(removeAccentofVogal(char).toLowerCase()) !== -1) {
+      return char.repeat(4)
+    } else {
+      return char
+    }
+  }).join('');
+
+  return (
+    <AnimatedText
+    type="chars" // animate words or chars
+    animationType="wave"
+    interval={0.3}
+    duration={4}
+    tag="p"
+    className={styles.karaokeFont}
+    includeWhiteSpaces
+    threshold={0.3}
+
+  >
+    {triplicado}
+  </AnimatedText>
+  )
+}
